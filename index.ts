afcd31b39bcf05c6c750aa0565f25b459826fcbf
Phil <phil.zhu@dhms.com.cn>
2022-05-12 09:39:05 +0800

    migrate to dayjs

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import moment from 'https://deno.land/x/momentjs@2.29.1-deno/mod.ts'
+import dayjs from 'https://cdn.skypack.dev/dayjs'
 import * as utils from './utils/index.ts'
 import { AlfredItem, AlfredItemMod } from './types/index.ts'
 
 function getNowTime(): Array<AlfredItem> {
-	const now = moment().clone()
+	const now = dayjs().clone()
 	return [
 		{
 			title: now.unix().toString(),
@@ -14,9 +14,9 @@ function getNowTime(): Array<AlfredItem> {
 			}
 		},
 		{
-			title: now.format('x').toString(),
-			subtitle: now.format('x').toString(),
-			arg: now.format('x').toString(),
+			title: now.valueOf(),
+			subtitle: now.valueOf(),
+			arg: now.valueOf(),
 			icon: {
 				path: './assets/now.png'
 			}
@@ -24,7 +24,7 @@ function getNowTime(): Array<AlfredItem> {
 		{
 			title: now.format("YYYY-MM-DD HH:mm:ss").toString(),
 			subtitle: now.format("YYYY-MM-DD HH:mm:ss").toString(),
-			arg: now.format('x').toString(),
+			arg: now.format("YYYY-MM-DD HH:mm:ss").toString(),
 			icon: {
 				path: './assets/now.png'
 			}
@@ -48,7 +48,7 @@ function parseTime(): Array<AlfredItem> {
 			throw 'Invalid timestcamp'
 		}
 
-		const parsedTime = moment(time.length == 10 ? +time * 1000 : +time).format("YYYY-MM-DD HH:mm:ss").toString()
+		const parsedTime = dayjs(time.length == 10 ? +time * 1000 : +time).utc().local().format("YYYY-MM-DD HH:mm:ss").toString()
 		return [{
 			title: parsedTime,
 			subtitle: `origin: ${time}`,
@@ -60,17 +60,17 @@ function parseTime(): Array<AlfredItem> {
 	} else {
 		return [
 			{
-				title: moment(time).format('X').toString(),
-				subtitle: moment(time).format('X').toString(),
-				arg: moment(time).format('X').toString(),
+				title: dayjs(time).unix().toString(),
+				subtitle: dayjs(time).unix().toString(),
+				arg: dayjs(time).unix().toString(),
 				icon: {
 					path: './assets/moment.png'
 				},
 			},
 			{
-				title: moment(time).format('x').toString(),
-				subtitle: moment(time).format('x').toString(),
-				arg: moment(time).format('x').toString(),
+				title: dayjs(time).valueOf().toString(),
+				subtitle: dayjs(time).valueOf().toString(),
+				arg: dayjs(time).valueOf().toString(),
 				icon: {
 					path: './assets/moment.png'
 				},
@@ -82,8 +82,7 @@ function parseTime(): Array<AlfredItem> {
 function main() {
 	const command = Deno.args[0]
 
-	const resultItems: Array<AlfredItem> = []	
-
+	const resultItems: Array<AlfredItem> = []
 	try {
 		if (command == 'now') {
 			resultItems.push(...getNowTime())
